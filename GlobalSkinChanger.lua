bc84e1bc174bcf84e282da741903ff4c2451310c
MrJuicyLemon <juanjimenezbleye@protonmail.com>
2016-06-13 20:55:35 +0200

    Update GlobalSkinChanger.lua

--- GlobalSkinChanger.lua
+++ GlobalSkinChanger.lua
@@ -1,6 +1,6 @@
 local Version = "1.00"
 function Update()
-	if GetGameTimer() > 120 then print("Game is already progressing, skipping Auto Update") return end
+	
 
 	local UpdateHost = "raw.githubusercontent.com"
 	local ServerPath = "/MrJuicyLemon/BoL/master/"
