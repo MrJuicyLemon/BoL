acf1a8abf2950b6cbcfefa12070a40b826ee14f1
MrJuicyLemon <juanjimenezbleye@protonmail.com>
2016-06-15 10:15:11 +0200

    Create Anti-AFK.lua

---
+++ Alpha/Anti-AFK.lua
@@ -0,0 +1,73 @@
+local GotMenu = 0
+function OnLoad()
+	do
+	  if (FileExist(LIB_PATH.."MenuConfig.lua")) then
+	    require 'MenuConfig'
+	  else
+	    GotMenu = 1
+	    message("Downloading MenuConfig, please don't reload script!")
+	    DownloadFile("https://raw.githubusercontent.com/linkpad/BoL/master/Common/MenuConfig.lua?rand="..math.random(1, 10000), LIB_PATH.."MenuConfig.lua", function()
+	      message("Finsihed downloading MenuConfig, please reload script!")
+	  end)
+	  
+	  if GotMenu == 1 then
+	  	LoadMenu()
+	  else
+	  	message("Library is being downloaded, please wait.")
+	  end
+	end
+end
+
+function OnUnLoad()
+	do
+		message("I hope to see you soon! Good day/night")
+	end
+end
+
+function LoadMenu()
+	do
+		AFK = MenuConfig("Anti AFK", "Anti AFK")
+		AFK:Menu("Settings", "Settings", "adjust-alt")
+		AFK.Settings:Slider("ClickDelay", "Click Delay (ms)", 200, 50, 2000, 50)
+		AFK.Settings:Boolean("SimulateClick", "Simulate Click with drawing", true)
+		AFK.Settings:Info("Command", "To enable Anti AFK write")
+		AFK.Settings:Info("Command2", "AFK ON to turn it on")
+		AFK.Settings:Info("Command3", "AFK OFF to turn it off")
+	end
+end
+
+local hasBeenDelayed = false
+
+function AntiAFK()
+	do
+	  myHero:MoveTo(myHero.x, myHero.y)
+	  if AFK.Settings.SimulateClick then
+	  	DrawCircle3D(myHero.x, myHero.y, myHero.z, 3, 2, ARGB(255, 255, 120, 78), 32)
+	  end
+	  DelayAction(hasBeenDelayed = false, (AFK.Settings.ClickDelay/1000)/2)
+	end
+end
+
+function OnSendChat(p)
+	do
+		if p = "AFK ON" then
+			BlockChat()
+			AfkMode = 1
+		end
+		if p = "AFK OFF" then
+			BlockChat()
+			AfkMode = 0
+		end
+	end
+end
+
+local AfkMode = 0
+function OnTick()
+	do
+		if AfkMode == 1 then
+		  if not hasBeenDelayed then
+		    DelayAction(AntiAFK(), (AFK.Settings.ClickDelay/1000)/2)
+		    hasBeenDelayed = true
+		  end
+		end
+end
