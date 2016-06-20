3d604f15b95aece96c8f7c2886b77d8fc6d3b7bb
MrJuicyLemon <juanjimenezbleye@protonmail.com>
2016-06-20 09:06:48 +0200

    Update EloCheck.lua

--- EloCheck.lua
+++ EloCheck.lua
@@ -1,5 +1,4 @@
-local Version = "1.01"
-
+local Version = "1.02"
 
 local CopyRight = "MrJuicyLemon"
 
@@ -171,39 +170,12 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 local socket = require("socket")
 local data2e, data2a
 
 local function MakeRequest(url)
     local tcp = socket.tcp()
+    url = url:gsub("%s", "%%20")
 	tcp:settimeout(5)
 	tcp:connect(GetGameRegion()..".op.gg", "80")
 	
@@ -219,60 +191,43 @@ local function MakeRequest(url)
 	end
 end
 
-print("Requesting data...")
---if (not data or data == "error" or not data:find("description")) then print("Error requesting data") return end
-for i = 1,6 do
-	if GetEnemyHeroes()[i] ~= nil then
-		datae = MakeRequest(GetEnemyHeroes()[i].name)
-		data2e = datae:find("description")
-		datae = datae:sub(data2e+22, data2e+80)
-		infoE = {}
-		datae:gsub('(.-)/', function(a)
-			table.insert(infoE, a)
-		end)
-	end
-	if GetAllyHeroes()[i] ~= nil then
-		dataa = MakeRequest(GetAllyHeroes()[i].name)
-		data2a = dataa:find("description")
-		dataa = dataa:sub(data2a+22, data2a+80)
-		infoA = {}
-		dataa:gsub('(.-)/', function(b)
-			table.insert(infoA, b)
-		end)
-	end
-
-
-	-----------------
-
-	
-
-	----------------------------------
-end
-
-
 function OnLoad()
-	do
-		if (FileExist(LIB_PATH.."MenuConfig.lua")) then
-	    require 'MenuConfig'
-	  else
-	    message("Downloading MenuConfig, please don't reload script!")
-	    DownloadFile("https://raw.githubusercontent.com/linkpad/BoL/master/Common/MenuConfig.lua?rand="..math.random(1, 10000), LIB_PATH.."MenuConfig.lua", function()
-	      message("Finsihed downloading MenuConfig, please reload script!")
-	  	end)
-	  end
-		Elo = MenuConfig("Elo Check", "Elo Check")
-				Elo:Section("Elo Check", ARGB(255, 114, 223, 230))
-				Elo:Menu("ally", "Allies", "gamepad")
-				Elo:Menu("enemy", "Enemies", "leaf")
-				for i = 1,6 do
-					if GetEnemyHeroes()[i] ~= nil then
-						Elo.enemy:Info(GetEnemyHeroes()[i].charName.." 's Division: "..infoE[2], "Info")
-						Elo.enemy:Info(GetEnemyHeroes()[i].charName.." 's Win/loss & Win%: "..infoE[3], "Info")
-					end
-					if GetAllyHeroes()[i] ~= nil then
-						Elo.ally:Info(GetAllyHeroes()[i].charName.." 's Division: "..infoA[2], "Info")
-						Elo.ally:Info(GetAllyHeroes()[i].charName.." 's Win/loss & Win%: "..infoA[3], "Info")
-					end
-				end
+	if (FileExist(LIB_PATH.."MenuConfig.lua")) then
+	require 'MenuConfig'
+  else
+	message("Downloading MenuConfig, please don't reload script!")
+	DownloadFile("https://raw.githubusercontent.com/linkpad/BoL/master/Common/MenuConfig.lua?rand="..math.random(1, 10000), LIB_PATH.."MenuConfig.lua", function()
+	  message("Finsihed downloading MenuConfig, please reload script!")
+	end)
+  end
+  Elo = MenuConfig("Elo Check", "Elo Check")
+	Elo:Section("Elo Check", ARGB(255, 114, 223, 230))
+	Elo:Menu("ally", "Allies", "gamepad")
+	Elo:Menu("enemy", "Enemies", "leaf")
+	print("Requesting data...")
+--if (not data or data == "error" or not data:find("description")) then print("Error requesting data") return end
+	for i = 1,6 do
+		if GetEnemyHeroes()[i] ~= nil then
+			datae = MakeRequest(GetEnemyHeroes()[i].name)
+			data2e = datae:find("description")
+			datae = datae:sub(data2e+22, data2e+100)
+			infoE = {}
+			datae:gsub('(.-)/', function(a)
+				table.insert(infoE, a)
+			end)
+			Elo.enemy:Info(GetEnemyHeroes()[i].charName.." 's Division: "..infoE[2], "Info")
+			Elo.enemy:Info(GetEnemyHeroes()[i].charName.." 's Win/loss & Win%: "..infoE[3], "Info")
+		end
+		if GetAllyHeroes()[i] ~= nil then
+			dataa = MakeRequest(GetAllyHeroes()[i].name)
+			data2a = dataa:find("description")
+			dataa = dataa:sub(data2a+22, data2a+100)
+			infoA = {}
+			dataa:gsub('(.-)/', function(b)
+				table.insert(infoA, b)
+			end)
+			Elo.ally:Info(GetAllyHeroes()[i].charName.." 's Division: "..infoA[2], "Info")
+			Elo.ally:Info(GetAllyHeroes()[i].charName.." 's Win/loss & Win%: "..infoA[3], "Info")
+		end
 	end
 end
